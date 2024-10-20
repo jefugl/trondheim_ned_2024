8e65f931a60db02a1a990d0f24dc8403aaa2c85f
Jan Erik Fuglseth <jefugl@gmail.com>
2024-10-20 13:40:14 +0200

    input data to the python file

---
+++ inputdata.py
@@ -0,0 +1,24 @@
+latitude = [59.93549344315811, 59.93161304636234, 59.92978865415876, 59.92937470874547, 59.92652338340753,
+            59.92262832492968, 59.921071303607526, 59.92006288485663, 59.91850221707881]
+
+longitude = [10.780295184660787, 10.783032708257378, 10.780314633800728, 10.778352966983343, 10.777583954781878,
+             10.769320899862388, 10.770385044083538, 10.766156071292523, 10.760228810326705]
+
+pubname = ['1.Renna-Rendevouz', '2,Wembley Sports Bar', '3.Buen Bar', '4.Valentino', '5.Carls', '6.Perla',
+           '7.Pane & Vino', '8.Ocean', '9. Schouskjelleren Mikrobryggeri']
+
+pubaddress = ['Trondheimsveien 187,0570 Oslo', 'Schouterrassen 25,0573 Oslo', 'Olaf Schous vei 2,0572 Oslo',
+              'Trondheimsveien 139a,0570 Oslo', 'Trondheimsveien 113, 0571 Oslo', 'Helgesens gate 66,0558 Oslo',
+              'Trondheimsveien 37,0560 Oslo', 'Trondheimsveien 20A,0560 Oslo', 'Trondheimsveien 2,0560,Oslo']
+
+renna = 'https://streetviewpixels-pa.googleapis.com/v1/thumbnail?panoid=fix1-Enollh1a7Fg3qAg3Q&cb_client=search.gws-prod.gps&w=408&h=240&yaw=131.19734&pitch=0&thumbfov=100'
+wembley = 'https://www.google.com/maps/place/Wembley+Sports+Bar+AS/@59.9316191,10.7828956,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipPyVismtayvoRa-hCh4EEP4-SjQNlPqaYRC6WM4!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipPyVismtayvoRa-hCh4EEP4-SjQNlPqaYRC6WM4%3Dw152-h86-k-no!7i1600!8i900!4m7!3m6!1s0x46416f74e1a676a5:0xd55936d423ccf498!8m2!3d59.9315628!4d10.7830083!10e5!16s%2Fg%2F11bw4vvgph?authuser=0&entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D#'
+buen = 'https://streetviewpixels-pa.googleapis.com/v1/thumbnail?panoid=Lj56C0glIVxYY8fMHZveSQ&cb_client=search.gws-prod.gps&w=408&h=240&yaw=295.29984&pitch=0&thumbfov=100'
+valentino = 'https://www.google.com/maps/place/Valentino+Pizzeria+og+Gastropub/@59.9293423,10.7783066,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipMlVZZC5SMPM_F526HUUu0h15YbJkVwlEWqz5bd!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMlVZZC5SMPM_F526HUUu0h15YbJkVwlEWqz5bd%3Dw397-h298-k-no!7i4000!8i3000!4m9!3m8!1s0x46416e46d6b6087d:0xd93a27d2ef7cd438!8m2!3d59.9293423!4d10.7783066!10e5!14m1!1BCgIgARICEAE!16s%2Fg%2F12mkt1gxh?authuser=0&entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D#'
+carls = 'https://lh5.googleusercontent.com/p/AF1QipNltJF9wOM7K9A-q3Gm0F3e5dDw28BKi9UUbLVr=w408-h664-k-no'
+perla = 'https://www.google.com/maps/place/Perla/@59.922512,10.7694175,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipOkBQMgdhtAU5VGKJ7yrp2mupZ4fUYJDVXKcIL0!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipOkBQMgdhtAU5VGKJ7yrp2mupZ4fUYJDVXKcIL0%3Dw203-h270-k-no!7i2448!8i3264!4m7!3m6!1s0x46416e42d61843e1:0x7baba13167d16eae!8m2!3d59.9226112!4d10.7695158!10e5!16s%2Fg%2F11bvvy8p2l?authuser=0&entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D#'
+pane_vino = 'https://www.google.com/maps/place/Pane+%26+Vino/@59.921124,10.7705589,3a,75y,229.25h,90t/data=!3m7!1e1!3m5!1sS9nVZj0s-srDL8Vrbjjj7A!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DS9nVZj0s-srDL8Vrbjjj7A%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D229.24722%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192!4m17!1m9!3m8!1s0x46416e5cdd9746fb:0x8a85cab421694226!2sTrondheimsveien+37,+0560+Oslo!3b1!8m2!3d59.9210759!4d10.7700713!10e5!16s%2Fg%2F11b8v6md9d!3m6!1s0x46416e5cda49cf49:0xf3fe75538a19faa1!8m2!3d59.9210382!4d10.7703637!10e5!16s%2Fg%2F1td2y1g5?authuser=0&coh=205409&entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D#'
+ocean = 'https://streetviewpixels-pa.googleapis.com/v1/thumbnail?panoid=Qt42jpJ1u4V7fbbQ9pmIyg&cb_client=search.gws-prod.gps&w=408&h=240&yaw=292.06885&pitch=0&thumbfov=100'
+schous = 'https://www.google.com/maps/place/Schouskjelleren+Mikrobryggeri/@59.9184737,10.7605833,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipOXopbqfWaX3_J95CdFP5Ogk1paqVZuwpDD8vDq!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipOXopbqfWaX3_J95CdFP5Ogk1paqVZuwpDD8vDq%3Dw203-h152-k-no!7i4032!8i3024!4m7!3m6!1s0x46416e673e0570f9:0x6ac95ebb184986c4!8m2!3d59.9184162!4d10.760261!10e5!16s%2Fg%2F1220fq5n?authuser=0&entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D#'
+
+#carls  59.92652338340753, 10.777583954781878
